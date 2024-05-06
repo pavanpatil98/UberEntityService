f23892190b060975c65ca4ce0e2705a7c1a7e85d
pavanpatil98 <pavanpatil271998@gmail.com>
2024-05-06 22:39:34 +0530

    Project Setup

---
+++ src/main/java/com/example/UberProjectEntityService/UberProjectEntityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.UberProjectEntityService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberProjectEntityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberProjectEntityServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/UberProjectEntityService/UberProjectEntityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UberProjectEntityService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberProjectEntityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
